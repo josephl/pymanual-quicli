2bc9b094bc6edefdd3b154b2cca6ec89f5450012
Joseph Lee <joe.lee.three.thousand@gmail.com>
2022-11-10 16:40:46 -0800

    Add arg to handle SIGINT to break on keyboard interrupt

--- quick.py
+++ quick.py
@@ -3,6 +3,7 @@
 import argparse
 from configparser import ConfigParser
 import logging
+import signal
 from time import time
 from typing import Any, Dict, Optional, Tuple
 
@@ -10,25 +11,41 @@ from typing import Any, Dict, Optional, Tuple
 def parse_args() -> Tuple[argparse.Namespace, ConfigParser]:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-c", "--config", action="append", help="Config path(s)",
+        "-c",
+        "--config",
+        action="append",
+        help="Config path(s)",
     )
     parser.add_argument(
         "-v", "--verbose", action="count", default=0, help="Verbose logging"
+    )
+    parser.add_argument(
+        "-b",
+        "--break-int",
+        action="store_true",
+        help="Capture SIGINT with breakpoint (Keyboard Interrupt/Ctrl+C)",
     )
     args = parser.parse_args()
     config = ConfigParser()
     if args.config:
         config.read(args.config)
-    logging_args = config["logging"] if "logging" in config else {
-        "format": "%(asctime)s %(levelname)s %(filename)s:%(funcName)s:%(lineno)d - %(message)s",
-    }
+    logging_args = (
+        config["logging"]
+        if "logging" in config
+        else {
+            "format": "%(asctime)s %(levelname)s %(filename)s:%(funcName)s:%(lineno)d - %(message)s",
+        }
+    )
     set_logging(vcount=args.verbose, **logging_args)
     return args, config
 
 
-def set_logging(
-    vcount: Optional[int] = 0, **log_args: Dict[str, Any]
-) -> None:
+def sigint_handler(sig, frame):
+    logging.exception(f"Handled signal {sig} at: {frame}")
+    breakpoint()
+
+
+def set_logging(vcount: Optional[int] = 0, **log_args: Dict[str, Any]) -> None:
     """
     Apply logging options to `logging.basicConfig`
     """
@@ -41,6 +58,9 @@ def set_logging(
 
 def main():
     args, config = parse_args()
+    if args.break_int:
+        signal.signal(signal.SIGINT, sigint_handler)
+    pass
 
 
 if __name__ == "__main__":
