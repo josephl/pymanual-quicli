28804acaf6f2aad3bf4dc793edc5ee465b0cadfc
Joseph Lee <joe.lee.three.thousand@gmail.com>
2023-03-01 12:27:50 -0800

    Remove sig-int breakpoint handler, wasn't using it as much as I thought

--- quick.py
+++ quick.py
@@ -3,7 +3,6 @@
 import argparse
 from configparser import ConfigParser
 import logging
-import signal
 from time import time
 from typing import Any, Dict, Optional, Tuple
 
@@ -19,16 +18,10 @@ def parse_args() -> Tuple[argparse.Namespace, ConfigParser]:
     parser.add_argument(
         "-v", "--verbose", action="count", default=0, help="Verbose logging"
     )
-    parser.add_argument(
-        "-b",
-        "--break-int",
-        action="store_true",
-        help="Capture SIGINT with breakpoint (Keyboard Interrupt/Ctrl+C)",
-    )
     args = parser.parse_args()
     config = ConfigParser()
-    if args.config:
-        config.read(args.config)
+    for config_path in args.config or []:
+        config.read(config_path)
     logging_args = (
         config["logging"]
         if "logging" in config
@@ -38,11 +31,6 @@ def parse_args() -> Tuple[argparse.Namespace, ConfigParser]:
     )
     set_logging(vcount=args.verbose, **logging_args)
     return args, config
-
-
-def sigint_handler(sig, frame):
-    logging.exception(f"Handled signal {sig} at: {frame}")
-    breakpoint()
 
 
 def set_logging(vcount: Optional[int] = 0, **log_args: Dict[str, Any]) -> None:
@@ -65,10 +53,15 @@ def main():
 
 if __name__ == "__main__":
     start = time()
+    caught_exc = None
     try:
         main()
-    except Exception:
+    except Exception as exc:
+        caught_exc = exc
         raise
     finally:
         duration = time() - start
-        logging.info(f"Completed running '{__file__}' in {duration:0.03f} seconds")
+        if caught_exc:
+            logging.exception(caught_exc)
+        prefix = "Failed" if caught_exc else "Completed"
+        logging.info(f"{prefix} running '{__file__}' in {duration:0.03f} seconds")
